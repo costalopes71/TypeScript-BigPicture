ae2773558b1625c4b26a65dc37dd5babdb4311d4
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-03-24 00:27:16 -0300

    iniciando com tsc e primeiros passos com typescript

---
+++ basic.ts
@@ -0,0 +1,21 @@
+class Customer {
+
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    announce() {
+        return "Hello, my name is " + this.name;
+    }
+
+}
+
+// create a new instance
+let firstCustomer = new Customer("Alice");
+let newH1: string = firstCustomer.announce();
+
+// change the text on the page
+let webHeading = document.querySelector('h1');
+webHeading!.textContent = newH1;
